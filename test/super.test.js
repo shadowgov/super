43e7805afad57a6870b1a9dac4880e6a106dfbec
Veselin Todorov <veselin@veselin.bg>
2012-01-04 15:36:41 +0200

    First commit

---
+++ test/super.test.js
@@ -0,0 +1,30 @@
+/*!
+ * super - Extend everything.
+ * 
+ * Author: Veselin Todorov <hi@vesln.com>
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Dependencies.
+ */
+var extend = require('../');
+var EventEmitter = require('events').EventEmitter;
+
+function Foo() {};
+
+describe('super', function() {
+  it('should inherit methods', function() {
+    extend(Foo, EventEmitter);
+    var foo = new Foo;
+    
+    (foo instanceof EventEmitter).should.be.true;
+    (Foo.super_ == EventEmitter).should.be.true;
+  });
+  
+  it('should extend objects', function() {
+    var foo = { bar: 'baz' };
+    extend({}, foo).should.eql({ bar: 'baz' });
+    extend(foo, { baz: 1 }).should.eql({ bar: 'baz', baz: 1 });
+  });
+});
