69e25ec6b51eb1541962bdfd2cd7aa467c1f5144
Jake Luer <jake.luer@gmail.com>
2012-06-13 16:21:37 -0400

    convert tests to use chai

--- test/super.test.js
+++ test/super.test.js
@@ -1,13 +1,21 @@
 /*!
  * super - Extend everything.
- * 
+ *
  * Author: Veselin Todorov <hi@vesln.com>
  * Licensed under the MIT License.
  */
 
 /**
- * Dependencies.
+ * Test Dependencies.
  */
+
+var chai = require('chai')
+  , should = chai.should();
+
+/**
+ * Module Dependancies
+ */
+
 var extend = require('../');
 var EventEmitter = require('events').EventEmitter;
 
@@ -17,17 +25,17 @@ describe('super', function() {
   it('should inherit methods', function() {
     extend(Foo, EventEmitter);
     var foo = new Foo;
-    
+
     (foo instanceof EventEmitter).should.be.true;
     (Foo.super_ == EventEmitter).should.be.true;
   });
-  
+
   it('should extend objects', function() {
     var foo = { bar: 'baz' };
     extend({}, foo).should.eql({ bar: 'baz' });
     extend(foo, { baz: 1 }).should.eql({ bar: 'baz', baz: 1 });
   });
-  
+
   it('should be able to be used as clone', function() {
     var foo = { bar: 'baz' };
     extend(foo).should.eql(foo);
