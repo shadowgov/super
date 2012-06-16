ec287fe54d08b40ae51e04f41ab5fe529f571f98
Jake Luer <jake.luer@gmail.com>
2012-06-15 23:57:44 -0400

    browser build

--- super.js
+++ super.js
@@ -9,7 +9,7 @@
   if (typeof module != 'undefined') module.exports = definition();
   else if (typeof define == 'function' && typeof define.amd  == 'object') define(definition);
   else this[name] = definition();
-}('extend', function () {
+}('inherits', function () {
   var module = {};
 
 
@@ -30,18 +30,23 @@ var slice = Array.prototype.slice;
  * Primary export
  */
 
-module.exports = super_;
+var exports = module.exports = super_;
 
 /*!
  * Module version
  */
 
-module.exports.version = '0.1.0';
+exports.version = '0.1.0';
 
 /**
  * ### _super (dest, orig)
  *
  * Inherits the prototype methods or merges objects.
+ * This is the primary export and it is recommended
+ * that it be imported as `inherits` in node to match
+ * the auto imported browser interface.
+ *
+ *     var inherits = require('super');
  *
  * @param {Object|Function} destination object
  * @param {Object|Function} source object
@@ -52,8 +57,48 @@ module.exports.version = '0.1.0';
 function super_ () {
   var args = slice.call(arguments);
   if (!args.length) return;
-  if (typeof args[0] !== 'function') return extend(args);
-  inherits.apply(null, args);
+  if (typeof args[0] !== 'function') return exports.merge(args);
+  exports.inherits.apply(null, args);
+};
+
+/**
+ * ### extend (proto[, klass])
+ *
+ * Provide `.extend` mechanism to allow extenion without
+ * needing to use dependancy.
+ *
+ *     function Bar () {
+ *       this._konstructed = true;
+ *     }
+ *
+ *     Bar.extend = inherits.extend;
+ *
+ *     var Fu = Bar.extend({
+ *       initialize: function () {
+ *         this._initialized = true;
+ *       }
+ *     });
+ *
+ *     var fu = new Fu();
+ *     fu.should.be.instanceof(Fu); // true
+ *     fu.should.be.instanceof(Bar); // true
+ *
+ * @param {Object} properties/methods to add to new prototype
+ * @param {Object} properties/methods to add to new class
+ * @returns {Object} new constructor
+ * @name extend
+ * @api public
+ */
+
+exports.extend = function (proto, klass) {
+  var self = this
+    , child = function () { return self.apply(this, arguments); };
+  exports.merge([ child, this ]);
+  exports.inherits(child, this);
+  if (proto) exports.merge([ child.prototype, proto ]);
+  if (klass) exports.merge([ child, klass ]);
+  child.extend = this.extend; // prevent overwrite
+  return child;
 };
 
 /*!
@@ -64,9 +109,10 @@ function super_ () {
  *
  * @param {Function} destination
  * @param {Function} source
+ * @api private
  */
 
-function inherits (ctor, superCtor) {
+exports.inherits = function (ctor, superCtor) {
   ctor.super_ = superCtor;
   ctor.prototype = Object.create(superCtor.prototype,
     { constructor: {
@@ -85,7 +131,7 @@ function inherits (ctor, superCtor) {
  * @api private
  */
 
-function extend (arr) {
+exports.merge = function (arr) {
   var main = arr.length === 2 ? arr.shift() : {};
   arr.forEach(function(obj) {
     for (var p in obj) {
